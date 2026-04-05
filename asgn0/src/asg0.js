24948bffd55f72b08bd19eca1e55039927bd5a56
tcle103 <146970839+tcle103@users.noreply.github.com>
2026-04-04 21:18:25 -0700

    added user interface for drawing v1

--- asgn0/src/asg0.js
+++ asgn0/src/asg0.js
@@ -1,7 +1,7 @@
 // Based on DrawTriangle.js (c) 2012 matsuda
 
-const origin = {x: 200, y: 200}
-const v1 = new Vector3([2.25, 2.25, 0])
+const origin = {x: 200, y: 200};
+const v1 = new Vector3([0, 0, 0]);
 
 function main() {  
   // Retrieve <canvas> element
@@ -14,16 +14,29 @@ function main() {
   // Get the rendering context for 2DCG
   var ctx = canvas.getContext('2d');
 
-  // // Draw a blue rectangle
-  // ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
-  // ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
+  /*
+  2. (1 point) Draw a red vector v1 on a black canvas instead of the blue rectangle.
+  The origin of the vector should be the center of the canvas. 
+  */
 
-  // Draw black canvas instead
+  // Draw black canvas
   ctx.fillStyle = 'rgba(0, 0, 0, 1.0)';
   ctx.fillRect(0, 0, 400, 400);
 
-  // Draw vector
-  drawVector(v1, "red", ctx)
+  // // Draw red vector v1
+  // v1.elements[0] = 2.25;
+  // v1.elements[1] = 2.25;
+  // drawVector(v1, "red", ctx)
+
+  /*
+  3. (1 point)  Add to your webpage an interface for the user to specify and draw 
+  the v1 vector.
+  */
+  const drawB = document.getElementById("btn");
+  const v1x = document.getElementById("v1x");
+  const v1y = document.getElementById("v1y");
+  drawB.addEventListener("click", (_) => handleDrawEvent(ctx, v1x ,v1y));
+
 }
 
 function drawVector(v, color, ctx) {
@@ -32,4 +45,16 @@ function drawVector(v, color, ctx) {
   ctx.moveTo(origin.x, origin.y);
   ctx.lineTo((origin.x + v.elements[0] * 20), (origin.y - v.elements[1] * 20));
   ctx.stroke();
+}
+
+function handleDrawEvent(ctx, v1x, v1y) {
+  // clear the canvas
+  ctx.clearRect(0, 0, 400, 400);
+  // Draw black canvas
+  ctx.fillStyle = 'rgba(0, 0, 0, 1.0)';
+  ctx.fillRect(0, 0, 400, 400);
+  // create v1
+  v1.elements[0] = v1x.value;
+  v1.elements[1] = v1y.value;
+  drawVector(v1, "red", ctx);
 }
