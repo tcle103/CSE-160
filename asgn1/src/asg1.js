cb331dc0a0e9d8a2d33c819fd5d8410e4c785b57
tcle103 <146970839+tcle103@users.noreply.github.com>
2026-04-15 20:29:13 -0700

    draw fish, move

--- asgn1/src/asg1.js
+++ asgn1/src/asg1.js
@@ -75,12 +75,15 @@ class Circle extends Point {
         gl.uniform1f(u_Size, size);
 
 
-        let r = this.size/ 400.0;
+        let r = this.size / 400.0;
         drawCircle(xy, gl, a_Position, this.seg, r);
     }
 }
-
-let currShape = "Point";
+let fancy = false;
+let currShape = "Fish?";
+let currPos = [0, 0];
+let targetPos = [0, 0];
+let dir = 1;
 
 function main() {
 
@@ -91,9 +94,14 @@ function main() {
     }
     // 8. Draw a shape when the mouse is held down and there is mouse motion
     canvas.onmousemove = (ev) => {
-        if (ev.buttons == 1) {
-            click(ev, gl, canvas, a_Position, u_FragColor, u_Size);
+        if (!fancy) {
+            if (ev.buttons == 1) {
+                click(ev, gl, canvas, a_Position, u_FragColor, u_Size);
+            }
+        } else {
+            setTarget(ev, canvas);
         }
+
     }
 
     // 7. Have a button to clear the canvas
@@ -104,15 +112,21 @@ function main() {
 
     let pButt = document.getElementById("p");
     pButt.onmousedown = (_) => {
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
         currShape = pButt.value;
+        fancy = false;
     }
     let tButt = document.getElementById("t");
     tButt.onmousedown = (_) => {
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
         currShape = tButt.value;
+        fancy = false;
     }
     let cButt = document.getElementById("c");
     cButt.onmousedown = (_) => {
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
         currShape = cButt.value;
+        fancy = false;
     }
 }
 
@@ -206,30 +220,43 @@ function click(ev, gl, canvas, a_Position, u_FragColor, u_Size) {
     */
     let segSlide = document.getElementById("seg");
 
-    let p;
-    switch (currShape) {
-        case "Triangle":
-            p = new Triangle([x, y], [rslide.value / 100,
-            gslide.value / 100,
-            bslide.value / 100,
-                1.0], sSlide.value);
-            break;
-        case "Circle":
-            p =  p = new Circle([x, y], [rslide.value / 100,
-            gslide.value / 100,
-            bslide.value / 100,
-                1.0], sSlide.value, segSlide.value);
-            break;
-        default:
-            p = new Point([x, y], [rslide.value / 100,
-            gslide.value / 100,
-            bslide.value / 100,
-                1.0], sSlide.value);
-            break;
+    if (!fancy) {
+        let p;
+        switch (currShape) {
+            case "Triangle":
+                p = new Triangle([x, y], [rslide.value / 100,
+                gslide.value / 100,
+                bslide.value / 100,
+                    1.0], sSlide.value);
+                break;
+            case "Circle":
+                p = p = new Circle([x, y], [rslide.value / 100,
+                gslide.value / 100,
+                bslide.value / 100,
+                    1.0], sSlide.value, segSlide.value);
+                break;
+            case "Fish?":
+                function draw() {
+                    fishTrack(gl, a_Position, u_FragColor, u_Size);
+                    if (fancy) {
+                        requestAnimationFrame(draw);
+                    }
+                }
+                requestAnimationFrame(draw);
+                fancy = true;
+                break;
+            default:
+                p = new Point([x, y], [rslide.value / 100,
+                gslide.value / 100,
+                bslide.value / 100,
+                    1.0], sSlide.value);
+                break;
+        }
+        if (p) {
+            shapesList.push(p);
+            renderAllShapes(gl, a_Position, u_FragColor, u_Size);
+        }
     }
-    shapesList.push(p);
-
-    renderAllShapes(gl, a_Position, u_FragColor, u_Size);
 }
 
 function clear(gl, a_Position, u_FragColor, u_Size) {
@@ -269,11 +296,110 @@ function drawCircle(pos, gl, a_Position, seg, r) {
     let deg1;
     let deg2;
     for (let i = 1; i < seg + 1; i++) {
-        deg1 = (360 * (i-1)/ seg) * (Math.PI / 180);
+        deg1 = (360 * (i - 1) / seg) * (Math.PI / 180);
         deg2 = (360 * i / seg) * (Math.PI / 180);
         v = [pos[0], pos[1],
-            r*Math.cos(deg1) + pos[0], r*Math.sin(deg1) + pos[1],
-            r*Math.cos(deg2) + pos[0], r*Math.sin(deg2) + pos[1] ];
+        r * Math.cos(deg1) + pos[0], r * Math.sin(deg1) + pos[1],
+        r * Math.cos(deg2) + pos[0], r * Math.sin(deg2) + pos[1]];
         drawTriangle(v, gl, a_Position);
     }
+}
+
+function drawRect(corners, gl, a_Position) {
+    drawTriangle(
+        [corners[0], corners[3],
+        corners[2], corners[1],
+        corners[0], corners[1]],
+        gl, a_Position);
+    drawTriangle(
+        [corners[0], corners[3],
+        corners[2], corners[3],
+        corners[2], corners[1]],
+        gl, a_Position);
+}
+
+function drawFancy(gl, a_Position, u_FragColor, u_Size) {
+    gl.clearColor(0.3, 0.4, 0.8, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+function drawFish(p, gl, a_Position, u_FragColor, u_Size) {
+    // Pass size of point to u_Size
+    gl.uniform1f(u_Size, 10.0);
+    let s = 0.1;
+    let d = dir * -1;
+
+    // body
+    gl.uniform4f(u_FragColor, 1.0, 0.4, 0.0, 1.0);
+
+    drawRect([p[0] + s * d, p[1] + (2 * s), p[0] + (3 * s * d), p[1] - s], gl, a_Position);
+    drawTriangle([p[0] + (3 * s * d), p[1] + s, p[0] + (4 * s * d), p[1] + s, p[0] + (3 * s * d), p[1] + (2 * s)],
+        gl, a_Position);
+    drawRect([p[0] + (3 * s * d), p[1] + s, p[0] + (4 * s * d), p[1]], gl, a_Position);
+    drawTriangle([p[0] + (3 * s * d), p[1] - s, p[0] + (4 * s * d), p[1], p[0] + (3 * s * d), p[1]],
+        gl, a_Position);
+
+    // fins
+    gl.uniform4f(u_FragColor, 1.0, 0.25, 0.0, 1.0);
+    drawTriangle([p[0] + s * d, p[1] + (2 * s), p[0] + (3 * s * d), p[1] + (2 * s), p[0] + (3 * s * d), p[1] + (3 * s)],
+        gl, a_Position);
+    drawTriangle([p[0] + s * d, p[1] - s, p[0] + (3 * s * d), p[1] - (s * 3 / 2), p[0] + (3 * s * d), p[1] - s],
+        gl, a_Position);
+    drawTriangle([p[0] + (4 * s * d), p[1] + s, p[0] + (5 * s * d), p[1] + s, p[0] + (5 * s * d), p[1] + (2 * s)],
+        gl, a_Position);
+    drawRect([p[0] + (4 * s * d), p[1] + s, p[0] + (5 * s * d), p[1]], gl, a_Position);
+    drawTriangle([p[0] + (4 * s * d), p[1], p[0] + (5 * s * d), p[1] - s, p[0] + (5 * s * d), p[1]],
+        gl, a_Position);
+
+    gl.uniform4f(u_FragColor, 1.0, 0.3, 0.0, 1.0);
+    drawTriangle([p[0] + (s * 3 / 2 * d), p[1] + s, p[0] + (2 * s * d), p[1] + s, p[0] + (2 * s * d), p[1] + (s * 3 / 2)],
+        gl, a_Position);
+    drawRect([p[0] + (s * 3 / 2 * d), p[1] + s, p[0] + (2 * s * d), p[1]], gl, a_Position);
+    drawTriangle([p[0] + (s * 3 / 2 * d), p[1], p[0] + (2 * s * d), p[1] - (s / 2), p[0] + (2 * s * d), p[1]],
+        gl, a_Position);
+
+    // head
+    // Pass the color of a point to u_FragColor variable
+    gl.uniform4f(u_FragColor, 1.0, 0.25, 0.0, 1.0);
+
+    drawTriangle([p[0], p[1], p[0] + s * d, p[1] - s, p[0] + s * d, p[1] + (2 * s)],
+        gl, a_Position);
+    drawTriangle([p[0] + s * d, p[1] + s, p[0] + (s * 3 / 2 * d), p[1] + s, p[0] + s * d, p[1] + (2 * s)],
+        gl, a_Position);
+    drawRect([p[0] + s * d, p[1] + s, p[0] + (s * 3 / 2 * d), p[1]], gl, a_Position);
+    drawTriangle([p[0] + s * d, p[1], p[0] + s * d, p[1] - s, p[0] + (s * 3 / 2 * d), p[1]], gl, a_Position);
+
+    gl.uniform4f(u_FragColor, 0.2, 0.0, 0.03, 1.0);
+    drawCircle([p[0] + (s * 0.8 * d), p[1] + (s * 0.6)], gl, a_Position, 8, 0.015);
+}
+
+function setTarget(ev, canvas) {
+    var x = ev.clientX; // x coordinate of a mouse pointer
+    var y = ev.clientY; // y coordinate of a mouse pointer
+    var rect = ev.target.getBoundingClientRect();
+
+    x = ((x - rect.left) - canvas.width / 2) / (canvas.width / 2);
+    y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2);
+
+    targetPos = [x, y];
+}
+
+function fishTrack(gl, a_Position, u_FragColor, u_Size) {
+    let x = targetPos[0];
+    let y = targetPos[1];
+
+    if (x < currPos[0]) {
+        dir = -1;
+    } else {
+        dir = 1;
+    }
+
+    yp = y - currPos[1];
+    xp = x - currPos[0];
+
+    currPos[0] += 0.02 * xp;
+    currPos[1] += 0.02 * yp;
+
+    drawFancy(gl, a_Position, u_FragColor, u_Size);
+    drawFish(currPos, gl, a_Position, u_FragColor, u_Size);
 }
